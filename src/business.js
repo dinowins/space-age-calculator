8b6ba1bfc0694106e792d66cfaf2e83342da132d
Dino Nguyen <dinodnguyen@gmail.com>
2019-03-15 13:41:46 -0700

    Add constructor function for calculating age on planets

---
+++ src/business.js
@@ -0,0 +1,14 @@
+class SpaceAge {
+  constructor(earthYears) {
+    this.earthAge = earthYears;
+    this.mercuryAge = earthYears * .24;
+    this.venusAge = earthYears * .62;
+    this.marsAge = earthYears * 1.88;
+    this.jupiterYears = earthYears * 11.86;
+  }
+
+  SpaceAge.prototype.checkLifeExpectancyLeft = function () {
+    const earthLifeExpectancy = 80;
+    const 
+  }
+}
