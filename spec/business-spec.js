030dcb41e132afabfb104ca3d67c43039b21f243
Dino Nguyen <dinodnguyen@gmail.com>
2019-03-17 19:59:26 -0700

    Add test for mercuryAge function

--- spec/business-spec.js
+++ spec/business-spec.js
@@ -1,9 +1,14 @@
 import { User } from "./../src/business.js"
 
 describe('User', function() {
-  const input = new User("1995-03-10");
 
-  it("Should calculate input age based on birthdate", function() {
-    expect(input.getEarthAge()).toEqual(24);
+  const user = new User("1995-03-10");
+
+  it("Should calculate input age based on birthdate", function(){
+    expect(user.earthAge()).toEqual(24);
+  });
+
+  it("Should calculate the Mercury age for the input age", function(){
+    expect(user.mercuryAge()).toEqual(100);
   });
 });
